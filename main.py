6417ec1cac57efd36cd47d0b15b9b3c29cafbbf2
alvin saini <codewithalvin@gmail.com>
2021-10-06 19:39:07 +0530

    scrapping done

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_fixtures(date:str):
+    """
+    scrape football fixture data from internet 
+    parse it append to a dictonary and return it
+
+    date: yyyyddmm [must be this format]
+    
+    """
+    url = f"https://www.espn.in/football/fixtures/_/date/{date}"
+    # date in format yyyyddmm
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    data={}
+
+    leauge_names = soup.find_all("h2",attrs={"class":"table-caption"})
+
+    for leauge in leauge_names:
+        lg_name = leauge.text
+        raw_data = leauge.next_sibling.findAll("tr",attrs={"class":["odd","even"]})
+
+        data[lg_name]=[]
+
+        for item in raw_data:
+            names = [abbr.get("title") for abbr in item.find_all("abbr")]
+            images = [img.get("src") for img in item.find_all("img")]
+            timing = [date.get("data-date") for date in item.find_all("td",attrs={"data-behavior":"date_time"})]
+            data[lg_name].append((names,images,timing))
+
+    return data
+
+print(get_fixtures("20211220"))
+
+
