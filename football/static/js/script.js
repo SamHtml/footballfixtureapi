48d5686e8388bec0f5f1463d1231c8fb0dffe04c
alvin saini <codewithalvin@gmail.com>
2021-10-12 15:46:18 +0530

    added filter method in frontend

---
+++ football/static/js/script.js
@@ -0,0 +1,20 @@
+let dates = document.getElementsByClassName('date')
+   Array.from(dates).forEach(element => {
+     if ( new Date(element.textContent)  != "Invalid Date"){
+      element.textContent = new Date(element.textContent).toLocaleTimeString().replace(":00","")
+     }
+   });
+
+let data = document.getElementById("data").children
+
+function filter(pattern){
+    Array.from(data).forEach(leauges => {
+        if (leauges.id != pattern){
+            leauges.hidden = true
+        }
+        else{
+            leauges.hidden = false
+        }
+
+    })
+}
